582cbe41ab903bc4f05510332e7d4007cdae4e32
unknown <nmacharashvili171993@gmail.com>
2023-12-16 17:40:15 -0800

    many changes made

--- js/hiddenAnimation.js
+++ js/hiddenAnimation.js
@@ -5,9 +5,11 @@ const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
+        entry.target.classList.remove('fade');
         entry.target.classList.add('active');
       } else {
         entry.target.classList.remove('active');
+        entry.target.classList.add('fade');
       }
     });
   },
