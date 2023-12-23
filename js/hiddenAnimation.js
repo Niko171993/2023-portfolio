d5279a6690b90f86abc4143e7398e0b6dfdb7272
unknown <nmacharashvili171993@gmail.com>
2023-12-22 23:11:02 -0800

    final touches for1

--- js/hiddenAnimation.js
+++ js/hiddenAnimation.js
@@ -1,23 +1,23 @@
-const hiddenElements = document.querySelectorAll('.entry');
-const debounceTime = 200; // Adjust the delay time as needed
+// const hiddenElements = document.querySelectorAll('.entry');
+// const debounceTime = 200; // Adjust the delay time as needed
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.remove('fade');
-        entry.target.classList.add('active');
-      } else {
-        entry.target.classList.remove('active');
-        entry.target.classList.add('fade');
-      }
-    });
-  },
-  { threshold: 0.5 }
-);
-hiddenElements.forEach((element) => observer.observe(element));
-// observer for the 1st section
-//header-info
-const headerInfo = document.querySelector('.hero-info');
-const heroImg = document.querySelector('.hero-img');
-const heroElements = [headerInfo, heroImg];
+// const observer = new IntersectionObserver(
+//   (entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.remove('fade');
+//         entry.target.classList.add('active');
+//       } else {
+//         entry.target.classList.remove('active');
+//         entry.target.classList.add('fade');
+//       }
+//     });
+//   },
+//   { threshold: 0.5 }
+// );
+// hiddenElements.forEach((element) => observer.observe(element));
+// // observer for the 1st section
+// //header-info
+// const headerInfo = document.querySelector('.hero-info');
+// const heroImg = document.querySelector('.hero-img');
+// const heroElements = [headerInfo, heroImg];
